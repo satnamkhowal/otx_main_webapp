6b62f93528d36827254a45669a6af321ec271837
satnamkhowal <satnamkhowal@gmail.com>
2020-09-12 07:02:07 +0530

    my first commit

---
+++ src/main/java/com/otx/OtxMianWebappApplication.java
@@ -0,0 +1,13 @@
+package com.otx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtxMianWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtxMianWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otx/OtxMianWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtxMianWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
